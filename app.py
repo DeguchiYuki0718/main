6576dcfbe8a174bd112956f90f894b249ae10e5d
DeguchiYuki0718 <y2002kia@icloud.com>
2024-12-19 16:18:03 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ def delete(id):
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
+    
     app.logger.info("Request body: " + body)
 
     try:
